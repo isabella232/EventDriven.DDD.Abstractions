a5db037a41661ea11322c4fda12b4cda8f51f381
Anthony Sneed <anthony.sneed@hilti.com>
2022-01-07 05:45:58 -0600

    Clean up Command ctor.

--- src/EventDriven.DDD.Abstractions/Commands/Command.cs
+++ src/EventDriven.DDD.Abstractions/Commands/Command.cs
@@ -3,18 +3,8 @@
 /// <summary>
 /// An object that is sent to the domain for a state change which is handled by a command handler.
 /// </summary>
-public record Command : ICommand
+public record Command(Guid EntityId = default) : ICommand
 {
-    /// <summary>
-    /// Command constructor.
-    /// </summary>
-    /// <param name="entityId">Represents the ID of the entity the command is in reference to.</param>
-    public Command(Guid entityId = default)
-    {
-        EntityId = entityId;
-
-    }
-
     /// <inheritdoc />
-    public Guid EntityId { get; }
+    public Guid EntityId { get; } = EntityId;
 }
