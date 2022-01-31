d156409646cd3147b1a457ab8a511783717f6f61
Anthony Sneed <anthony.sneed@hilti.com>
2022-01-31 06:38:31 -0600

    Update ICommandProcessor.Process to return a single IDomainEvent.

--- src/EventDriven.DDD.Abstractions/Commands/ICommandProcessor.cs
+++ src/EventDriven.DDD.Abstractions/Commands/ICommandProcessor.cs
@@ -3,15 +3,32 @@
 namespace EventDriven.DDD.Abstractions.Commands;
 
 /// <summary>
-/// Processes a command by generating one or more domain events.
+/// Processes a command by generating a domain event.
 /// </summary>
 /// <typeparam name="TCommand">Command type.</typeparam>
 public interface ICommandProcessor<in TCommand> where TCommand : class, ICommand
 {
     /// <summary>
-    /// Process specified command by creating one or more domain events.
+    /// Process specified command by creating a domain event.
     /// </summary>
     /// <param name="command">The command to process.</param>
-    /// <returns>Domain events resulting from the command.</returns>
-    IEnumerable<IDomainEvent> Process(TCommand command);
+    /// <returns>Domain event resulting from the command.</returns>
+    IDomainEvent Process(TCommand command);
+}
+
+/// <summary>
+/// Processes a command by generating a domain event.
+/// </summary>
+/// <typeparam name="TCommand">Command type.</typeparam>
+/// <typeparam name="TDomainEvent">Domain event type.</typeparam>
+public interface ICommandProcessor<in TCommand, out TDomainEvent>
+    where TCommand : class, ICommand
+    where TDomainEvent : IDomainEvent
+{
+    /// <summary>
+    /// Process specified command by creating a domain event.
+    /// </summary>
+    /// <param name="command">The command to process.</param>
+    /// <returns>{TDomainEvent} event resulting from the command.</returns>
+    TDomainEvent Process(TCommand command);
 }
